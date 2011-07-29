6d1ae549441ad9a34f136d401a05da42ba1f43b5
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2011-07-29 15:59:01 +0000

    mingw32 changes; ported to ossp uuid API for mingw, which is different from the Linux/OSX/Solaris standard from Ted Ts'o, but still not working
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/module-uuid/trunk@4264 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- src/uuid-module.h
+++ src/uuid-module.h
@@ -42,17 +42,38 @@ typedef char* uuid_string_t;
 #define QUF_TIME        4
 #define QUF_EMPTY       5
 
+#ifdef OSSP_UUID
+typedef uuid_t* q_uuid_t;
+#else
+typedef uuid_t q_uuid_t;
+#endif
+
+#define Q_UUID_LEN 37
+
 class QoreUUID : public AbstractPrivateData {
 protected:
-   uuid_t uuid;
+   q_uuid_t uuid;
 
-   DLLLOCAL static QoreStringNode *getStringIntern(uuid_t uuid, int flags = QUF_NONE) {
+   DLLLOCAL static QoreStringNode *getStringIntern(q_uuid_t uuid, int flags = QUF_NONE) {
       QoreStringNode *str = new QoreStringNode;
-      str->allocate(37);
+      str->allocate(Q_UUID_LEN + 1);
 
       if (flags == QUF_NONE)
          flags = QUF_LOWER_CASE;
 
+#ifdef OSSP_UUID
+      size_t len = Q_UUID_LEN;
+#ifdef DEBUG
+      uuid_rc_t rc = uuid_export(uuid, UUID_FMT_STR, (char *)str->getBuffer(), &len);
+      assert(rc == UUID_RC_OK);
+#else
+      uuid_export(uuid, UUID_FMT_STR, (char *)str->getBuffer(), &len);
+#endif
+      if (flags & QUF_UPPER_CASE)
+         str->toupr();
+      else
+         str->tolwr();
+#else // OSSP_UUID
 #ifdef HAVE_UUID_UNPARSE_CASE
       if (flags & QUF_LOWER_CASE)
          uuid_unparse_lower(uuid, (char *)str->getBuffer());
@@ -67,11 +88,18 @@ protected:
       else
          str->tolwr();
 #endif
+#endif // OSSP_UUID
 
       return str;
    }
 
-   DLLLOCAL static void generateIntern(uuid_t uuid, int flags = QUF_NONE) {
+   DLLLOCAL static void generateIntern(q_uuid_t uuid, int flags = QUF_NONE) {
+#ifdef OSSP_UUID
+      if (flags & QUF_TIME)
+         uuid_make(uuid, UUID_MAKE_V1);
+      else if (!(flags & QUF_EMPTY))
+         uuid_make(uuid, UUID_MAKE_V4);
+#else
       if (flags & QUF_RANDOM)
          uuid_generate_random(uuid);
       else if (flags & QUF_TIME)
@@ -80,38 +108,70 @@ protected:
          uuid_clear(uuid);
       else
          uuid_generate(uuid);
+#endif
    }
 
 public:
    DLLLOCAL QoreUUID(int flags = QUF_NONE) {
+#ifdef OSSP_UUID
+      uuid_create(&uuid);
+#endif
       generateIntern(uuid, flags);
    }
 
    DLLLOCAL QoreUUID(const QoreString &uuid_str, ExceptionSink *xsink) {
+#ifdef OSSP_UUID
+      uuid_create(&uuid);
+#endif
       set(uuid_str, xsink);
    }
 
    DLLLOCAL QoreUUID(const QoreUUID &old) {
+#ifdef OSSP_UUID
+      uuid_create(&uuid);
+      uuid_clone(old.uuid, &uuid);
+#else
       uuid_copy(uuid, const_cast<QoreUUID &>(old).uuid);
+#endif
    }
+
+#ifdef OSSP_UUID
+   DLLLOCAL ~QoreUUID() {
+      uuid_destroy(uuid);
+   }
+#endif
 
    DLLLOCAL void generate(int flags = QUF_NONE) {
       generateIntern(uuid, flags);
    }
 
+   DLLLOCAL QoreStringNode *getErrorString(ExceptionSink *xsink, const QoreString &uuid_str, const char *err = "cannot parse string") {
+      QoreStringNode *desc = new QoreStringNode(err);
+      desc->concat(": argument '");
+      desc->concat(&uuid_str, xsink);
+      if (desc->strlen() > 255) {
+         desc->terminate(255);
+         desc->concat("...");
+      }
+      desc->concat("' as a UUID string (must be in format like ex: '1b4e28ba-2fa1-11d2-883f-b9a761bde3fb')");
+      return desc;
+   }
+
    DLLLOCAL int set(const QoreString &uuid_str, ExceptionSink *xsink) {
+#ifdef OSSP_UUID
+      uuid_rc_t rc = uuid_import(uuid, UUID_FMT_STR, uuid_str.getBuffer(), uuid_str.strlen());
+      if (rc == UUID_RC_OK)
+         return 0;
+
+      xsink->raiseException("UUID-CONSTRUCTOR-ERROR", getErrorString(xsink, uuid_str, uuid_error(rc)));
+      return -1;
+#else
       if (uuid_parse((char *)uuid_str.getBuffer(), uuid)) {
-         QoreStringNode *desc = new QoreStringNode("cannot parse string argument '");
-         desc->concat(&uuid_str, xsink);
-         desc->concat("' as a UUID string (must be in format like ex: '1b4e28ba-2fa1-11d2-883f-b9a761bde3fb')");
-         if (desc->strlen() > 255) {
-            desc->terminate(255);
-            desc->concat("...");
-         }
-         xsink->raiseException("UUID-CONSTRUCTOR-ERROR", desc);
+         xsink->raiseException("UUID-CONSTRUCTOR-ERROR", getErrorString(xsink, uuid_str));
          return -1;
       }
       return 0;
+#endif
    }
 
    DLLLOCAL QoreStringNode *toString(int flags = QUF_NONE) {
@@ -119,19 +179,36 @@ public:
    }
 
    DLLLOCAL bool isNull() const {
+#ifdef OSSP_UUID
+      int result;
+      uuid_isnil(uuid, &result);
+      return result;
+#else
       return uuid_is_null(const_cast<QoreUUID *>(this)->uuid);
+#endif
    }
 
    DLLLOCAL void clear() {
+#ifdef OSSP_UUID
+      uuid_destroy(uuid);
+      uuid_create(&uuid);
+#else
       uuid_clear(uuid);
+#endif
    }
 
    DLLLOCAL int compare(const QoreUUID &other) {
+#ifdef OSSP_UUID
+      int result;
+      uuid_compare(uuid, other.uuid, &result);
+      return result;
+#else
       return uuid_compare(uuid, const_cast<QoreUUID &>(other).uuid);
+#endif
    }
 
    DLLLOCAL static QoreStringNode *get(int string_flags = QUF_NONE, int gen_flags = QUF_NONE) {
-      uuid_t uuid;
+      q_uuid_t uuid;
       generateIntern(uuid, gen_flags);
       return getStringIntern(uuid, string_flags);
    }
